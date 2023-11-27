5d96ceac1531a8c9b468629cf9ff67f26494dc80
mstrueby <122125717+mstrueby@users.noreply.github.com>
2023-11-27 23:25:00 +0000

    add logo upload in add-club

--- routers/clubs.py
+++ routers/clubs.py
@@ -1,11 +1,32 @@
 import os
-from typing import List
-from fastapi import APIRouter, Request, Body, status, HTTPException, Depends
+from typing import List, Optional
+from fastapi import (
+  APIRouter,
+  Request,
+  Body,
+  status,
+  HTTPException,
+  Depends,
+  Form,
+  File,
+  UploadFile,
+)
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from models.clubs import ClubBase, ClubDB, ClubUpdate
 from authentication import AuthHandler
 from pymongo.errors import DuplicateKeyError
+import cloudinary
+import cloudinary.uploader
+
+CLOUD_NAME = os.getenv("CLDY_CLOUD_NAME")
+API_KEY = os.getenv("CLDY_API_KEY")
+API_SECRET = os.getenv("CLDY_API_SECRET")
+cloudinary.config(
+  cloud_name=CLOUD_NAME,
+  api_key=API_KEY,
+  api_secret=API_SECRET,
+)
 
 router = APIRouter()
 auth = AuthHandler()
@@ -31,10 +52,55 @@ async def list_venues(
 # create new club
 @router.post("/", response_description="Add new club")
 async def create_club(
-    request: Request,
-    club: ClubBase = Body(...),
-    userId=Depends(auth.auth_wrapper),
+  request: Request,
+  #club: ClubBase = Body(...),
+  name: str = Form("name"),
+  alias: str = Form("alias"),
+  addressName: Optional[str] = Form(None),
+  street: Optional[str] = Form(None),
+  zipCode: Optional[str] = Form(None),
+  city: Optional[str] = Form(None),
+  country: str = Form("country"),
+  email: Optional[str] = Form(None),
+  yearOfFoundation: Optional[int] = Form(None),
+  description: Optional[str] = Form(None),
+  website: Optional[str] = Form(None),
+  ishdId: Optional[int] = Form(None),
+  active: Optional[bool] = Form(False),
+  logo: Optional[UploadFile] = File(None),
+  
+  userId=Depends(auth.auth_wrapper),
 ):
+  if logo:
+    result = cloudinary.uploader.upload(
+      logo.file,
+      folder="logos",
+      public_id=f"{alias}",
+      overwrite=True,
+      crop="scale",
+      height=300,
+    )
+    url = result.get("url")
+  else:
+    url = None
+
+  club = ClubDB(
+    name=name,
+    alias=alias,
+    addressName=addressName,
+    street=street,
+    zipCode=zipCode,
+    city=city,
+    country=country,
+    email=email,
+    # if yearOfFoundation is empty, set it to None
+    yearOfFoundation = yearOfFoundation,
+    description=description,
+    website=website,
+    ishdId=ishdId,
+    active=active,
+    logo=url,
+  )
   club = jsonable_encoder(club)
 
   try:
@@ -61,17 +127,59 @@ async def get_club(alias: str, request: Request):
 # Update club
 @router.patch("/{id}", response_description="Update club")
 async def update_club(
-    request: Request,
-    id: str,
-    club: ClubUpdate = Body(...),
-    userId=Depends(auth.auth_wrapper),
+  request: Request,
+  id: str,
+  #club: ClubBase = Body(...),
+  name: str = Form("name"),
+  alias: str = Form("alias"),
+  addressName: str = Form("addressName"),
+  street: str = Form("street"),
+  zipCode: str = Form("zipCode"),
+  city: str = Form("city"),
+  country: str = Form("country"),
+  email: str = Form("email"),
+  yearOfFoundation: int = Form("yearOfFoundation"),
+  description: str = Form("description"),
+  website: str = Form("website"),
+  ishdId: int = Form("ishdId"),
+  active: bool = Form("active"),
+  logo: UploadFile = File(...),
+
+  userId=Depends(auth.auth_wrapper),
 ):
-  club = club.dict(exclude_unset=True)
+  result = cloudinary.uploader.upload(
+    logo.file,
+    folder="logos",
+    public_id=f"{alias}",
+    overwrite=True,
+    crop="scale",
+    height=300,
+  )
+  url = result.get("url")
+  
+  club = ClubDB(
+    name=name,
+    alias=alias,
+    addressName=addressName,
+    street=street,
+    zipCode=zipCode,
+    city=city,
+    country=country,
+    email=email,
+    yearOfFoundation=yearOfFoundation,
+    description=description,
+    website=website,
+    ishdId=ishdId,
+    active=active,
+    logo=url,
+  )
+  club = jsonable_encoder(club)
+  
   exisitng_club = await request.app.mongodb["clubs"].find_one({"_id": id})
   if exisitng_club is None:
     raise HTTPException(status_code=404, detail=f"Club with id {id} not found")
   #Exclude unchanged data
-  club_to_update = {k: v for k, v in club.items() if v != exisitng_club.get(k)}
+  club_to_update = {k: v for k, v in club.items() if v != exisitng_club.get(k) and k != '_id'}
   if not club_to_update:
     return ClubDB(**exisitng_club)
   try:
