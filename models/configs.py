e229d7958416a53d6b7e798f4cecc81a43f6d8a0
mstrueby <122125717+mstrueby@users.noreply.github.com>
2024-06-10 14:06:57 +0000

    use model for config

--- models/configs.py
+++ models/configs.py
@@ -1,51 +1,13 @@
-from bson import ObjectId
-from datetime import datetime, date
-from pydantic import Field, BaseModel, HttpUrl, validator
-from typing import Optional, List
+# models/configs.py
+from pydantic import BaseModel
+from typing import List, Union
 
+class ConfigValue(BaseModel):
+    key: str
+    value: Union[str, int, float]  # Adjust types as needed
+    sortOrder: int
 
-class PyObjectId(ObjectId):
-
-  @classmethod
-  def __get_validators__(cls):
-    yield cls.validate
-
-  @classmethod
-  def validate(cls, v):
-    if not ObjectId.is_valid(v):
-      raise ValueError("Invalid objectid")
-    return ObjectId(v)
-
-  @classmethod
-  def __modify_schema__(cls, field_schema):
-    field_schema.update(type="string")
-
-
-class MongoBaseModel(BaseModel):
-  id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-
-  class Config:
-    json_encoders = {ObjectId: str}
-
-
-class ConfigDocBase(MongoBaseModel):
-  key: str = Field(..., description="The key of the age group")
-  value: str = Field(..., description="The value of the age group")
-  sortOrder: int = Field(..., description="The sort order of the age group")
-
-
-# -------------------
-
-
-class ConfigBase(MongoBaseModel):
-  name: str = Field(..., description="The name of the config")
-  value: ConfigDocBase = {}
-
-
-class ConfigDB(ConfigBase):
-  pass
-
-
-class ConfigUpdate(MongoBaseModel):
-  name: Optional[str] = "DEFAULT"
-  value: Optional[ConfigDocBase] = {}
+class Config(BaseModel):
+    key: str
+    name: str
+    value: List[ConfigValue]
