014d503b124a3117dc4055b7a95b8e21bbb9389e
mstrueby <122125717+mstrueby@users.noreply.github.com>
2024-06-13 15:17:46 +0000

    BSH-25 add patch endpoint for matches

--- routers/matches.py
+++ routers/matches.py
@@ -15,12 +15,12 @@ auth = AuthHandler()
 
 
 # get one match by id
-@router.get("/{id}", response_description="Get one match by id")
-async def get_match(request: Request, id: str) -> MatchDB:
+@router.get("/{match_id}", response_description="Get one match by id")
+async def get_match(request: Request, match_id: str) -> MatchDB:
   if (match := await
-      request.app.mongodb["matches"].find_one({"_id": id})) is not None:
+      request.app.mongodb["matches"].find_one({"_id": match_id})) is not None:
     return MatchDB(**match)
-  raise HTTPException(status_code=404, detail=f"Match with id {id} not found")
+  raise HTTPException(status_code=404, detail=f"Match with id {match_id} not found")
 
 
 # create new match
@@ -28,23 +28,23 @@ async def get_match(request: Request, id: str) -> MatchDB:
 async def create_match(
   request: Request,
   match: MatchBase = Body(...),
-  #userId=Depends(auth.auth_wrapper),
+  user_id=Depends(auth.auth_wrapper),
 ) -> MatchDB:
-  matchData = my_jsonable_encoder(match)
-  print("matchData: ", matchData)
+  match_data = my_jsonable_encoder(match)
+  print("match_data: ", match_data)
   
   # remove some attibutes from match
-  matchHeader = matchData.copy()
-  print("matchData 2 : ", matchData)
+  match_header = match_data.copy()
+  print("match_data 2 : ", match_data)
   
-  matchHeader['homeTeam'].pop('roster', None)
-  matchHeader['awayTeam'].pop('roster', None)
-  matchHeader.pop('scoreEvents', None)
-  matchHeader.pop('penaltyEvents', None)
-  print("reduced match: ", matchHeader)
+  match_header['homeTeam'].pop('roster', None)
+  match_header['awayTeam'].pop('roster', None)
+  match_header.pop('scoreEvents', None)
+  match_header.pop('penaltyEvents', None)
+  print("reduced match: ", match_header)
   
-  # renew matchData, because is some how modified by copy()
-  matchData = my_jsonable_encoder(match)
+  # renew match_data, because is some how modified by copy()
+  match_data = my_jsonable_encoder(match)
   
   try:
     
@@ -52,10 +52,10 @@ async def create_match(
     filter = {"alias": match.tournament.alias}
     update = {
       "$push": {
-        "seasons.$[s].rounds.$[r].matchdays.$[md].matches": (matchHeader)
+        "seasons.$[s].rounds.$[r].matchdays.$[md].matches": (match_header)
       }
     }
-    arrayFilters = [
+    array_filters = [
       {
         "s.alias": match.season.alias
       },
@@ -68,11 +68,11 @@ async def create_match(
     ]
     print("do update")
     print("update tournament: ", update)
-    print("arryFilters: ", arrayFilters)
+    print("arryFilters: ", array_filters)
     result = await request.app.mongodb["tournaments"].update_one(
       filter=filter,
       update=update,
-      array_filters=arrayFilters,
+      array_filters=array_filters,
       upsert=False)
     if result.modified_count == 0:
       raise HTTPException(
@@ -83,20 +83,111 @@ async def create_match(
 
     # Second: add match to collection matches
     print("insert into matches") 
-    print("matchData: ", matchData)
+    print("match_data: ", match_data)
     
-    result = await request.app.mongodb["matches"].insert_one(matchData)
-    newMatch = await request.app.mongodb["matches"].find_one(
+    result = await request.app.mongodb["matches"].insert_one(match_data)
+    new_match = await request.app.mongodb["matches"].find_one(
       {"_id": result.inserted_id})
 
     # lastly: return complete match document
-    if newMatch:
-      newMatchModel = MatchDB(**newMatch)
+    if new_match:
+      new_match_model = MatchDB(**new_match)
       return JSONResponse(status_code=status.HTTP_201_CREATED,
-                          content=jsonable_encoder(newMatchModel))
+                          content=jsonable_encoder(new_match_model))
     else:
       raise HTTPException(status_code=500, detail="Failed to create match")
   
 
   except Exception as e:
     raise HTTPException(status_code=500, detail=str(e))
+
+# update match
+@router.patch("/{match_id}", response_description="Update match")
+async def update_match(
+  request: Request,
+  match_id: str,
+  match: MatchUpdate = Body(...),
+  user_id=Depends(auth.auth_wrapper)
+) -> MatchDB:
+  print("match: ", match)
+  match_data = match.dict(exclude_unset=True)
+  match_data.pop("id", None)
+  print("match_data: ", match_data)
+
+  existing_match = await request.app.mongodb["matches"].find_one({"_id": match_id})
+  if existing_match is None:
+    raise HTTPException(status_code=404, detail=f"Match with id {match_id} not found")
+
+  # exclude unchanged data
+  match_to_update = {
+    k: v
+    for k, v in match_data.items() if v != existing_match.get(k)
+  }
+
+  if match_to_update:
+    try:
+      print("match to update: ", match_to_update)
+      update_result = await request.app.mongodb["matches"].update_one(
+        {"_id": match_id}, {"$set": match_data})
+      print("update result: ", update_result.modified_count)
+      if update_result.modified_count == 0:
+        raise HTTPException(
+          status_code=404,
+          detail=f"Match with id {match_id} not found")
+
+      else:
+        # update match header data in tournament/matchday/matches
+        print("second update")
+        match_header = match_data.copy()
+        if 'homeTeam' in match_header and 'roster' in match_header['homeTeam']:
+            match_header['homeTeam'].pop('roster')
+        if 'awayTeam' in match_header and 'roster' in match_header['awayTeam']:
+            match_header['awayTeam'].pop('roster')
+        if 'scoreEvents' in match_header:
+            match_header.pop('scoreEvents')
+        if 'penaltyEvents' in match_header:
+            match_header.pop('penaltyEvents')
+        print("reduced match: ", match_header)
+        print("existing match: ", existing_match)
+        
+      if match_header is not None:
+        filter = {"alias": existing_match['tournament']['alias']}
+        print("filter: ", filter)
+        update = {
+            "$push": {
+                "seasons.$[s].rounds.$[r].matchdays.$[md].matches": (match_header)
+            }
+        }
+        array_filters = [
+            {"s.alias": existing_match['season']['alias']},
+            {"r.alias": existing_match['round']['alias']},
+            {"md.alias": existing_match['matchday']['alias']},
+        ]
+        print("do update")
+        print("update tournament: ", update)
+        print("arryFilters: ", array_filters)
+        result = await request.app.mongodb["tournaments"].update_one(
+            filter=filter,
+            update=update,
+            array_filters=array_filters,
+            upsert=False
+        )
+        if result.modified_count == 0:
+            raise HTTPException(
+                status_code=404,
+                detail=
+                f"Matchday with alias {existing_match['matchday']['alias']} not found in round {existing_match['round']['alias']} of season {existing_match['season']['alias']} of tournament {existing_match['tournament']['alias']}"
+            )
+
+    except Exception as e:
+      raise HTTPException(status_code=500, detail=str(e))
+  else:
+    print("No changes to update")
+
+  # return updated match
+  updated_match = await request.app.mongodb["matches"].find_one({"_id": match_id})
+  if updated_match is not None:
+    match_response = MatchDB(**updated_match)
+    return JSONResponse(status_code=status.HTTP_200_OK,content=jsonable_encoder(match_response))
+  else:
+    raise HTTPException(status_code=500, detail=f"Fetch: Match with id {match_id} not found")
