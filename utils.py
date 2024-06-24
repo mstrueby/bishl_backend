2def3cc6cd521e899a183d26fbbc08e67a48002c
mstrueby <122125717+mstrueby@users.noreply.github.com>
2024-06-24 13:10:21 +0000

    BSH-37 refactored validator functions, use util

--- utils.py
+++ utils.py
@@ -2,11 +2,14 @@ from datetime import datetime, time
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
+
 def parse_date(date_str):
     return datetime.strptime(date_str, '%Y-%m-%d') if date_str else None
 
+
 def parse_datetime(datetime_str):
-    return datetime.strptime(datetime_str, '%Y-%m-%d %H:%M:%S') if datetime_str else None
+    return datetime.strptime(datetime_str,
+                             '%Y-%m-%d %H:%M:%S') if datetime_str else None
 
 
 def parse_time_to_seconds(time_str):
@@ -20,6 +23,7 @@ def parse_time_from_seconds(seconds):
     minutes = seconds // 60
     seconds = seconds % 60
     return f"{minutes}:{seconds:02d}"
+
 
 def my_jsonable_encoder(obj):
     result = {}
@@ -38,3 +42,30 @@ def my_jsonable_encoder(obj):
             continue
         result[field_name] = jsonable_encoder(val)
     return result
+
+
+def empty_str_to_none(v, field_name: str):
+    if v == "":
+        print(
+            f"Field '{field_name}' is an empty string and has been set to None."
+        )
+        return None
+    return v
+
+
+def prevent_empty_str(v, field_name: str):
+    if v is None or v == "":
+        raise ValueError(
+            f"Field '{field_name}' cannot be null or empty string")
+    return v
+
+
+def validate_dict_of_strings(v, field_name: str):
+    if not isinstance(v, dict):
+        raise ValueError(f"Field '{field_name}' must be a dictionary")
+    for key, value in v.items():
+        if not isinstance(key, str) or not isinstance(value, str):
+            raise ValueError(
+                f"Field '{field_name}' must be a dictionary with string key-value pairs"
+            )
+    return v
