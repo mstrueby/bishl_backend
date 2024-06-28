5acbd6387e67bc533c0a18b6748c1aa7580f022f
mstrueby <122125717+mstrueby@users.noreply.github.com>
2024-06-28 20:13:34 +0000

    BSH-25 fix workflow match with apply_points

--- utils.py
+++ utils.py
@@ -102,45 +102,85 @@ async def fetch_standings_settings(tournament_alias):
           )
         return (await response.json()).get('standingsSettings')
 
-def apply_points(match, standingsSetting):
-    match match.finishType.get('key'):
-      case 'OVERTIME':
-        if match.home.stats.goalsFor > match.home.stats.goalsAgainst:
-          match.home.stats.otWin = 1
-          match.home.stats.points = standingsSetting.get("pointsWinOvertime")
-          match.away.stats.otLoss = 1
-          match.away.stats.points = standingsSetting.get("pointsLossOvertime")
+
+def apply_points(finishType, matchStats, standingsSetting):
+    stats = {
+        'home': {},
+        'away': {}
+    }
+    def reset_points():
+        stats['home']['points'] = 0
+        stats['home']['win'] = 0
+        stats['home']['loss'] = 0
+        stats['home']['draw'] = 0
+        stats['home']['otWin'] = 0
+        stats['home']['otLoss'] = 0
+        stats['home']['soWin'] = 0
+        stats['home']['soLoss'] = 0
+        stats['away']['points'] = 0
+        stats['away']['win'] = 0
+        stats['away']['loss'] = 0
+        stats['away']['draw'] = 0
+        stats['away']['otWin'] = 0
+        stats['away']['otLoss'] = 0
+        stats['away']['soWin'] = 0
+        stats['away']['soLoss'] = 0
+
+    # reassign goals
+    stats['home']['goalsFor'] = matchStats.get('goalsFor', 0)
+    stats['home']['goalsAgainst'] = matchStats.get('goalsAgainst', 0)
+    stats['away']['goalsFor'] = matchStats.get('goalsAgainst', 0)
+    stats['away']['goalsAgainst'] = matchStats.get('goalsFor', 0)
+
+    # matchStats goals are always for the home team!
+    if finishType.get('key') == 'REGULAR':
+        reset_points()
+        if stats['home']['goalsFor'] > stats['away']['goalsFor']:
+            # home team wins in regulation
+            stats['home']['win'] = 1
+            stats['home']['points'] = standingsSetting.get("pointsWinReg")
+            stats['away']['loss'] = 1
+            stats['away']['points'] = standingsSetting.get("pointsLossReg")
+        elif stats['home']['goalsFor'] < stats['away']['goalsFor']:
+            # away team wins in regulation
+            stats['home']['loss'] = 1
+            stats['home']['points'] = standingsSetting.get("pointsLossReg")
+            stats['away']['win'] = 1
+            stats['away']['points'] = standingsSetting.get("pointsWinReg")
         else:
-          match.home.stats.otLoss = 1
-          match.home.stats.points = standingsSetting.get("pointsLossOvertime")
-          match.away.stats.otWin = 1
-          match.away.stats.points = standingsSetting.get("pointsWinOvertime")
-      case 'SHOOTOUT':
-        if match.home.stats.goalsFor > match.home.stats.goalsAgainst:
-          match.home.stats.soWin = 1
-          match.home.stats.points = standingsSetting.get("pointsWinShootout")
-          match.away.stats.soLoss = 1
-          match.away.stats.points = standingsSetting.get("pointsLossShootout")
+            # draw
+            stats['home']['draw'] = 1
+            stats['home']['points'] = standingsSetting.get("pointsDrawReg")
+            stats['away']['draw'] = 1
+            stats['away']['points'] = standingsSetting.get("pointsDrawReg")
+    elif finishType.get('key') == 'OVERTIME':
+        reset_points()
+        if stats['home']['goalsFor'] > stats['away']['goalsFor']:
+            # home team wins in OT
+            stats['home']['otWin'] = 1
+            stats['home']['points'] = standingsSetting.get("pointsWinOvertime")
+            stats['away']['otLoss'] = 1
+            stats['away']['points'] = standingsSetting.get("pointsLossOvertime")
         else:
-          match.home.stats.soLoss = 1
-          match.home.stats.points = standingsSetting.get("pointsLossShootout")
-          match.away.stats.soWin = 1
-          match.away.stats.points = standingsSetting.get("pointsWinShootout")
-      case 'REGULAR':
-        if match.home.stats.goalsFor > match.home.stats.goalsAgainst:
-          match.home.stats.win = 1
-          match.home.stats.points = standingsSetting.get("pointsWinReg")
-          match.away.stats.loss = 1
-          match.away.stats.points = standingsSetting.get("pointsLossReg")
-        elif match.home.stats.goalsFor < match.home.stats.goalsAgainst:
-          match.home.stats.loss = 1
-          match.home.stats.points = standingsSetting.get("pointsLossReg")
-          match.away.stats.win = 1
-          match.away.stats.points = standingsSetting.get("pointsWinReg")
+            # away team wins in OT
+            stats['home']['otLoss'] = 1
+            stats['home']['points'] = standingsSetting.get("pointsLossOvertime")
+            stats['away']['otWin'] = 1
+            stats['away']['points'] = standingsSetting.get("pointsWinOvertime")
+    elif finishType.get('key') == 'SHOOTOUT':
+        reset_points()
+        if stats['home']['goalsFor'] > stats['away']['goalsFor']:
+            # home team wins in shootout
+            stats['home']['soWin'] = 1
+            stats['home']['points'] = standingsSetting.get("pointsWinShootout")
+            stats['away']['soLoss'] = 1
+            stats['away']['points'] = standingsSetting.get("pointsLossShootout")
         else:
-          match.home.stats.draw = 1
-          match.home.stats.points = standingsSetting.get("pointsDrawReg")
-          match.away.stats.draw = 1
-          match.away.stats.points = standingsSetting.get("pointsDrawReg")
-      case _:
+            # away team wins in shootout
+            stats['home']['soLoss'] = 1
+            stats['home']['points'] = standingsSetting.get("pointsLossShootout")
+            stats['away']['soWin'] = 1
+            stats['away']['points'] = standingsSetting.get("pointsWinShootout")
+    else:
         print("Unknown finishType")
+    return stats
