7727531ddbeb521758a4d115b6305c49cbeec98b
mstrueby <122125717+mstrueby@users.noreply.github.com>
2024-07-09 16:14:55 +0000

    BSH-38 finish POST endpoint

--- routers/assignments.py
+++ routers/assignments.py
@@ -11,7 +11,9 @@ auth = AuthHandler()
 BASE_URL = os.environ['BE_API_URL']
 
 
-@router.post("/", response_model=AssignmentDB, response_description="")
+@router.post("/",
+             response_model=AssignmentDB,
+             response_description="create an initial assignment")
 async def create_assignment(
   request: Request,
   match_id: str = Path(..., description="Match ID"),
@@ -28,28 +30,55 @@ async def create_assignment(
     return await request.app.mongodb["assignments"].find_one(
       {"_id": insert_response.inserted_id})
 
-  user_id = token_payload.sub
-  if token_payload.roles not in [["ADMIN"], ['REFEREE'], ['REF_ADMIN']]:
-    raise HTTPException(status_code=403, detail="Not authorized")
+  if not any(role in ['ADMIN', 'REFEREE', 'REF_ADMIN']
+             for role in token_payload.roles):
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                        detail="Not authorized")
 
+  user_id = token_payload.sub
+  ref_id = assignment_data.user_id
+
+  # preliminary checks
+
+  # check if match exists
+  match = await request.app.mongodb["matches"].find_one({"_id": match_id})
+  if not match:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Match with id {match_id} not found")
   # check if assignment already exists for match_id and referee.user_id = user_id
   if await request.app.mongodb["assignments"].find_one({
       "match_id":
       match_id,
       "referee.user_id":
-      user_id
+      ref_id
   }):
     raise HTTPException(
       status_code=status.HTTP_409_CONFLICT,
       detail=
-      f"Assignment already exists for match_id {match_id} and referee.user_id {user_id}"
+      f"Assignment already exists for match_id {match_id} and referee.user_id {ref_id}"
     )
-
-  if user_id == assignment_data.user_id:
+  # check if referee user_id exists
+  ref_user = await request.app.mongodb["users"].find_one({"_id": ref_id})
+  if not ref_user:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="User not found")
+  # check if any role in ref_user is REFEREE
+  if 'REFEREE' not in ref_user.get('roles', []):
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="User is not a referee")
+  #print("roles", token_payload.roles)
+  #print("REF_ADMIN", any(role in ['ADMIN', 'REF_ADMIN'] for role in token_payload.roles))
+  if user_id == ref_id and 'REFEREE' in token_payload.roles and assignment_data.status != Status.assigned:
     # REFEREE mode
+    print("REFEREE mode")
+    if 'REFEREE' not in token_payload.roles:
+      raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                          detail="You are not a referee")
+
     if assignment_data.status not in [Status.requested, Status.unavailable]:
       raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
                           detail="Invalid assignment status")
+
     referee = {}
     referee["user_id"] = assignment_data.user_id
     referee["firstname"] = token_payload.firstname
@@ -67,25 +96,30 @@ async def create_assignment(
       raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                           detail="Assignment not created")
 
-  elif token_payload.roles in [["ADMIN"], ["REF_ADMIN"]]:
+  elif any(role in ['ADMIN', 'REF_ADMIN'] for role in token_payload.roles) and assignment_data.status == Status.assigned:
     # user_id != assignment_data.user_id
     # REF_ADMIN mode
     print("REF_ADMN mode")
-    if assignment_data.status not in [Status.assigned]:
+    
+    # Check if position is set in the assignment data
+    if not assignment_data.position:
       raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                          detail="Invalid assignment status")
-    # TODO: get referee data from col. users and pass to create_and_store_assignment
-    ref_user = await request.app.mongodb["users"].find_one(
-      {"_id": assignment_data.user_id})
-    if not ref_user:
-      raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                          detail="User not found")
+                          detail="Position must be set for this assignment")
+
+    if 'club' in ref_user and ref_user['club'] is not None:
+      club_id = ref_user['club']['club_id']
+      club_name = ref_user['club']['club_name']
+    else:
+      club_id = None
+      club_name = None
+
     referee = {}
+    referee["position"] = assignment_data.position
     referee["user_id"] = assignment_data.user_id
     referee["firstname"] = ref_user["firstname"]
     referee["lastname"] = ref_user["lastname"]
-    referee["club_id"] = ref_user["club"]["club_id"]
-    referee["club_name"] = ref_user["club"]["club_name"]
+    referee["club_id"] = club_id
+    referee["club_name"] = club_name
 
     new_assignment = await insert_assignment(match_id, referee,
                                              assignment_data.status)
@@ -98,4 +132,5 @@ async def create_assignment(
                           detail="Assignment not created")
 
   else:
-    raise HTTPException(status_code=403, detail="Not authorized")
+    raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                        detail="Invalid assignment status")
