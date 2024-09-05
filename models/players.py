96df80519375b6cfd8af59f14fbf93acd8bc07d6
mstrueby <122125717+mstrueby@users.noreply.github.com>
2024-09-05 15:02:45 +0000

    BSH-45 refactor models and ishd_processing

--- models/players.py
+++ models/players.py
@@ -33,21 +33,41 @@ class PositionEnum(str, Enum):
   SKATER = 'Skater'
   GOALIE = 'Goalie'
 
+class SourceEnum(str, Enum):
+  ISHD = 'ISHD'
+  BISHL = 'BISHL'
+
 class PlayerTeams(BaseModel):
-  name: str = Field(...)
+  team_id: str = Field(...)
+  team_name: str = Field(...)
+  team_alias: str = Field(...)
+  team_ishd_id: str = Field(...)
+  pass_no: str = Field(...)
+  source: SourceEnum = Field(default=SourceEnum.BISHL)
+  modify_date: datetime = None
+
 
 class PlayerClubs(BaseModel):
+  club_id: str = Field(...)
   club_name: str = Field(...)
-  club_ishd_id: int = Field(...)
+  club_alias: str = Field(...)
+  #club_ishd_id: int = Field(...)
   teams: list[PlayerTeams] = Field(...)
 
+
+class AssignmentInput(BaseModel):
+  club_id: str = Field(...)
+  teams: list[dict[str, str]] = Field(...)
+  
 class PlayerBase(MongoBaseModel):
   firstname: str = Field(...)
   lastname: str = Field(...)
   birthdate: datetime = Field(..., description='format: yyyy-mm-dd')
   nationality: str = None
   position: PositionEnum = Field(default=PositionEnum.SKATER)
-  team_assignments: List[PlayerClubs] = []
+  full_face_req: bool = False
+  source: SourceEnum = Field(default=SourceEnum.BISHL)
+  assignments: List[PlayerClubs] = []
   image: HttpUrl = None
   legacyId: int = None
 
@@ -60,8 +80,7 @@ class PlayerBase(MongoBaseModel):
     return empty_str_to_none(v, field.name)
 
 class PlayerDB(PlayerBase):
-  download_date: datetime = None
-  modify_date: datetime = None
+  create_date: datetime = None
 
 class PlayerUpdate(MongoBaseModel):
   firstname: Optional[str] = "DEFAULT"
@@ -69,9 +88,9 @@ class PlayerUpdate(MongoBaseModel):
   birthdate: Optional[datetime] = "DEFAULT"
   nationality: Optional[str] = None
   position: Optional[PositionEnum] = None
-  download_date: Optional[datetime] = None
-  modify_date: Optional[datetime] = "DEFAULT"
-  team_assignments: Optional[List[PlayerClubs]] = None
+  full_face_req: Optional[bool] = False
+  source: Optional[SourceEnum] = None
+  assignments: Optional[List[PlayerClubs]] = None
   image: Optional[HttpUrl] = None
 
   @validator('firstname', 'lastname', 'position', pre=True, always=True)
