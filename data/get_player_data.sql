3584e9f0c3137e124794792fb65e66ae6b04c8b9
mstrueby <122125717+mstrueby@users.noreply.github.com>
2024-09-23 14:23:21 +0000

    BSH-23 migrate players

---
+++ data/get_player_data.sql
@@ -0,0 +1,13 @@
+SELECT 
+  py_id,
+  firstname, 
+  lastname, 
+  concat(date_format(birthday, '%Y-%m-%d'), ' 00:00:00') as birthdate,
+  coalesce(nation, Nationality) as nationality,
+  case when isGoalie = 1 then 'Goalie' else 'Skater' end as player_position,
+  case when fullfacereq = 1 then 'True' else 'False' end as full_face_req,
+  'BISHL' as source,
+  id_tblPlayer as legacy_id
+FROM `tblplayer` 
+where dayname(birthday) is not null
+ORDER BY firstname, lastname ASC
